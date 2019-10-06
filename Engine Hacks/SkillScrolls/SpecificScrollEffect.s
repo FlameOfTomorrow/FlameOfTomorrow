45d00b76b1a5eead20a0de616e06b6df53edcc10
Sme <esahig15789@gmail.com>
2019-10-06 00:21:42 -0400

    Actually fixed skill books lmao

--- Engine Hacks/SkillScrolls/SpecificScrollEffect.s
+++ Engine Hacks/SkillScrolls/SpecificScrollEffect.s
@@ -25,23 +25,23 @@ mov r5,r1
 
 @char struct is in r0, item slot is in r1, use together to get item uses & char ID
 
-mov 		r2,r4
-add 		r2,#0x1E
-lsl 		r1,r1,#1
-add 		r2,r1
+mov 		r2,r4 @get char struct
+add 		r2,#0x1E @get start of inventory data
+lsl 		r1,r1,#1 @multiply item slot by 2, for length of inventory entry
+add 		r2,r1 @r2 = offset of item entry
 
 
 @delete the item from the inventory
 
 mov r0,#0
-strh r1,[r2]
-mov r0,r4
-blh RemoveUnitBlankItems,r3
+strh r0,[r2] @store 0x0000 to the item entry in question, thus removing it
+mov r0,r4 @r0 = char struct
+blh RemoveUnitBlankItems,r3 @move everything else up
 
 
-mov r0,r4
-mov r2,r6
-ldr r1,SkillID
+mov r0,r4 @r0 = char
+mov r2,r6 @r2 = parent proc
+ldr r1,SkillID @r1 = skill ID
 
 @now learn the skill specified in item uses
 
